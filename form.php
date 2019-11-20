ffe01b985897fc568fb15ff0880c7ae568b6f5a6
Divyansh Kamboj <kambojdivyansh2000@gmail.com>
2019-11-20 21:11:24 +0530

    get this fucking over with (#14)
    * Fuck me, I'm shit
    
    Signed-off-by: Divyansh Kamboj <kambojdivyansh2000@gmail.com>
    
    * FuCk
    
    Signed-off-by: Divyansh Kamboj <kambojdivyansh2000@gmail.com>

---
+++ form.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+
+<html>
+<head>
+    <title> Form </title>
+</head>
+
+<body>
+<?php
+$form_UUID = $_GET["f"];
+// get the the questions after getting the form uuid;
+?>
+</body>
+</html>
--- save_new_form.php
+++ save_new_form.php
@@ -21,14 +21,14 @@ $conn->query($insert);
 
 foreach($new_questions as $question){
         $question = mysqli_real_escape_string($conn, $question);
-        $insert = "INSERT INTO form_question values ('$form_UUID','$question');
+        $insert = "INSERT INTO form_question values ('$form_UUID','$question')";
         $conn->query($insert);
 }
 
 // TODO: create form URL
+$form_url = "http://" . $_SERVER['SERVER_NAME'] . "/form?f=" . $form_UUID;
+echo $form_url;
 
-// add the form UUID with the username in a username_form table
-// then add all the questions to the form_question table
 // generate a link (using form uuid)
 // find a way to open all the questions from form UUID
 // that is find a way to read the URL and fetch content
