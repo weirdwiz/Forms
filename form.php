363ec417bbf349a6020da7fe930af32893092af7
Divyansh Kamboj <kambojdivyansh2000@gmail.com>
2019-11-20 22:12:49 +0530

    idk why it's showing 3 commits (#15)
    * Fuck me, I'm shit
    
    Signed-off-by: Divyansh Kamboj <kambojdivyansh2000@gmail.com>
    
    * FuCk
    
    Signed-off-by: Divyansh Kamboj <kambojdivyansh2000@gmail.com>
    
    * dhoom machale dhoom
    
    Signed-off-by: Divyansh Kamboj <kambojdivyansh2000@gmail.com>

--- form.php
+++ form.php
@@ -7,8 +7,38 @@
 
 <body>
 <?php
+$servername = "localhost";
+$username = "root";
+$password = "12345@Qwert";
+$dbname = "form";
+
 $form_UUID = $_GET["f"];
 // get the the questions after getting the form uuid;
+
+$conn = new mysqli($servername, $username, $password, $dbname);
+
+if ($conn->connect_error) {
+        die("Connection failed: " . $conn->connect_error);
+}
+?>
+
+<div>
+<!-- needs styling -->
+
+<form action = "" method="POST">
+
+<?php
+
+$result = $conn->query("SELECT question from form_question where uuid=$form_UUID");
+while($row = $result->fetch_assoc()){
+        echo "<h1>".$row["question"]."</h1>;
+}
+?>
+</form>
+</div>
+
+<?php
+$conn->close();
 ?>
 </body>
 </html>
